927e117a4df40d2765b8b512615084f9f7b9eb0a
Dolcwan <51341410+Dolcwan@users.noreply.github.com>
2020-05-09 01:00:02 -0700

    updated shell script

--- Beta_Module/sb.sh
+++ Beta_Module/sb.sh
@@ -1,4 +1,16 @@
-f90 $F90FLAGS -o run -s -w super_beta_muon.f $LINK_FNL
+FILERUN=$PWD/run
+COMPILE_RUN="f90 $F90FLAGS -o run -s -w super_beta_muon.f $LINK_FNL"
+
+if [ -f "$FILERUN" ]
+then
+    runexit=$true
+else
+    runexit=$false
+fi
+
+if [ !$runexit ]; then
+    eval $COMPILE_RUN
+fi
+
 ./run
-rm run 
 echo "Done!"
